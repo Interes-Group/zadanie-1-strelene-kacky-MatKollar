a8ee0d77120972dc63f9e0eb9abf04e3342f008b
MatKollar <matej.kollar3@gmail.com>
2022-03-20 00:53:19 +0100

    Add usage of the Duck March card

--- src/main/java/sk/stuba/fei/uim/oop/tiles/cards/action/DuckMarchCard.java
+++ src/main/java/sk/stuba/fei/uim/oop/tiles/cards/action/DuckMarchCard.java
@@ -2,6 +2,10 @@ package sk.stuba.fei.uim.oop.tiles.cards.action;
 
 import sk.stuba.fei.uim.oop.board.Pond;
 import sk.stuba.fei.uim.oop.player.Player;
+import sk.stuba.fei.uim.oop.tiles.cards.Card;
+import sk.stuba.fei.uim.oop.tiles.packs.RiverPack;
+
+import java.util.ArrayList;
 
 public class DuckMarchCard extends ActionCard {
 
@@ -11,6 +15,11 @@ public class DuckMarchCard extends ActionCard {
 
     @Override
     public void activate(Pond pond, Player[] players) {
+        ArrayList<Card> riverCards = pond.getRiverCards();
+        RiverPack riverPack = pond.getRiverPack();
 
+        riverPack.cards.add(riverCards.get(0));
+        riverCards.remove(0);
+        pond.addCardOnRiver();
     }
 }
